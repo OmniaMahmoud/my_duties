1545b9168fcd95247043cc6fc0ae09ea0d7f1d00
Omnia Mahmoud <omnia.mahmoud@objects.ws>
2020-10-15 21:03:08 +0200

    Initial commit

---
+++ app/src/main/java/com/example/myduties/SplashActivity.java
@@ -0,0 +1,26 @@
+package com.example.myduties;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 3000);
+    }
+}
